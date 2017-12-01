57ba802db1c5fa4a5c8951e1d9251de5eac63cc0
Mark Blanford <markytorture@hotmail.com>
2017-12-01 11:51:59 +0000

    first layout - version A

---
+++ CSS Tools_ Reset CSS_files/theme.php
@@ -0,0 +1,3 @@
+/* image referenced in '#sitemast' rule Copyright Eric A. Meyer */
+#sitemast {background-image: url(/ui/themes/130823-withlove.jpg); background-position: center;}
+#sitemast h1 a {color: rgba(138,119,47,0.75); text-shadow: 0 0 3px rgba(105,66,27,0.75);}
